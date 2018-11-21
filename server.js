3b3c1770a6382866b2ea68709c004c161e5797a7
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-11-21 21:49:23 +0100

    Editing socket

--- server.js
+++ server.js
@@ -149,6 +149,7 @@ app.get('/memberinfo', (req, res) => {
 
 io.on('connection', function(socket) {
     socket.broadcast.emit('test',{msg: 'pykło tak sobie'});
+    io.emit('test',{msg:'pykykyk'});
     console.log('a user connected');
     socket.on('disconnect', function() {
         console.log('user disconnected');
