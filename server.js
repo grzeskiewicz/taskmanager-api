697de23c9ba4c8bcb6147fd37c37681e81d17438
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2019-01-14 21:07:08 +0100

    Adding db

--- server.js
+++ server.js
@@ -282,7 +282,7 @@ io.on('connection', function(socket) {
         });
 
         timer = setInterval(() => {
-            console.log('timer', task);
+            console.log(task['timeleft']);
             if (task['timeleft'] === 0 && task['status'] !== 'cancelled') {
                 task['status'] = 'timesup';
                 //task['timeleft'] = 0;
