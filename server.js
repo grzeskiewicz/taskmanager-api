cdd56c7da3eda43e4e9a589fc524fc0384e935a2
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2019-01-15 14:23:03 +0100

    Adding db

--- server.js
+++ server.js
@@ -211,7 +211,7 @@ function updateTaskDb(task) {
 function importTasksDb(username) {
     const dayBeginning=new Date();
     const dayEnd = new Date(dayBeginning.getTime() + 60 * 60 * 24 * 1000);
-    return Task.find({ username: username })
+    return Task.find({ username: username, date: { $gt: dayBeginning, $lt: dayEnd } })
    // .where('date').gt(dayBeginning).lt(dayEnd)
         .then((tasks) => {
             return tasks
