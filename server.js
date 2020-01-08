095a9833cb736219d5502325d0234f3c8c1ea06b
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2020-01-08 22:45:39 +0100

    Adding delete user

--- server.js
+++ server.js
@@ -138,6 +138,7 @@ app.post('/resetpassword',
 
 app.post('/deleteuser',
     (req, res) => {
+        console.log(req);
         const errors = validationResult(req);
         if (errors.isEmpty()) {
             const user = req.body;
