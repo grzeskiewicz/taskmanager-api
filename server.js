680962754718e7591dfb085cdd686a9e990761f5
grzeskiewicz <benuch91@gmail.com>
2020-07-28 22:43:02 +0200

    tests

--- server.js
+++ server.js
@@ -631,6 +631,23 @@ io.on('connection', function (socket) {
 
     });
 
+
+    socket.on('reset', function (task) {
+        console.log('reset', task);
+        task['status'] = 'pending';
+        task['timetoaccept'] = 0;
+        task['timeleft'] = 240;
+        updateTaskDb(task).then(() => {
+            importTasksDb(task.username).then((tasks) => {
+                io.to(`/admin-room`).emit('reset', tasks);
+                io.to(`/${task.username}-room`).emit('reset', tasks);
+               // clearInterval(timer);
+               // clearInterval(acceptTimer);
+            });
+        });
+
+    });
+
     socket.on('logout', function (user) {
         userlist.delete(user);
         //  console.log(userlist);
